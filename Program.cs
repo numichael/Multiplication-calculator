785cc2027c7dd488b3502fd303091aa85a5c6fcd
numichael <numichael.dev@gmail.com>
2021-08-24 23:23:53 +0100

    Initial commit pushing Division Calculator over to github

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the third number: ");
+            thirdNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber * secondNumber * thirdNumber;
+            Console.WriteLine("The answer is: " + result);
+        }
+    }
+}
